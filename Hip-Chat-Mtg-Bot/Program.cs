859dbfc1d466a07988e8463afa412aa605a68468
L0G1C <ljerez@mail.usf.edu>
2015-03-30 10:46:38 -0400

    Init Commit

---
+++ Hip-Chat-Mtg-Bot/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hip_Chat_Mtg_Bot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var apiKey = "c7d2ef15f55d05ea84da26925f3bd8";
+            var apiKey = "hhJa1dKNwKv9COBhDlg2miyifJzZ63qufiycaB0n";
+            var client = new HipchatApiV2.HipchatClient(apiKey);
+
+            client.SendNotification("MTG", "Test From MTG Bot");
+        }
+    }
+}
